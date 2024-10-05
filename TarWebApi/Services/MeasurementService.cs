080c338b2887f55e02c1d73a3fbb133faac7ddf5
torakis <56877228+torakis@users.noreply.github.com>
2024-10-05 12:57:17 +0300

    Update MeasurementService.cs

--- TarWebApi/Services/MeasurementService.cs
+++ TarWebApi/Services/MeasurementService.cs
@@ -2,6 +2,7 @@
 using System.Linq.Expressions;
 using Amazon.Runtime.Internal;
 using Microsoft.VisualBasic;
+using MongoDB.Bson;
 using MongoDB.Driver;
 using TarWebApi.Models;
 using TarWebApi.Models.Contracts;
@@ -49,16 +50,71 @@ public class MeasurementService : IMeasurementService
         var resp = new GetMeasurementByPeriodResponse() { IsSuccessful = true, ErrorText = "" };
         try
         {
+            // Define the filter for the query
             var filter = Builders<Measurement>.Filter.Where(s => s.StationId == request.StationId
                 && s.Date >= request.DateFrom
                 && s.Date <= request.DateTo);
 
-            var projection = GetMeasurementProjection(request.MeasurementType, new ArgumentOutOfRangeException());
+            var dateDiff = (request.DateTo - request.DateFrom)?.TotalDays ?? 0;
 
-            var measurements = await _measurementsCollection
-                .Find(filter)
-                .Project(projection)
-                .ToListAsync();
+            // Determine the grouping interval based on the date difference
+            string interval;
+            if (dateDiff < 1)
+            {
+                interval = "hour";  // Group by hour if the date range is less than 1 day
+            }
+            else if (dateDiff >= 1 && dateDiff < 7)
+            {
+                interval = "4hours";  // Group by 4 hours if the date range is between 1 and 7 days
+            }
+            else
+            {
+                interval = "day";  // Group by day if the date range is greater than 7 days
+            }
+
+            // Map the selected MeasurementType to the corresponding field in the database
+            string measurementField = request.MeasurementType switch
+            {
+                MeasurementType.Temperature => "Temperature",
+                MeasurementType.Humidity => "Humidity",
+                MeasurementType.Pressure => "Pressure",
+                MeasurementType.WindSpeed => "WindSpeed",
+                MeasurementType.WindDirection => "WindDirection",
+                MeasurementType.Gust => "Gust",
+                MeasurementType.Precipitation => "Precipitation",
+                MeasurementType.UVI => "UVI",
+                MeasurementType.Light => "Light",
+                MeasurementType.CO2 => "CO2",
+                _ => throw new ArgumentOutOfRangeException(nameof(request.MeasurementType), "Unsupported MeasurementType")
+            };
+
+            // Build the aggregation pipeline
+            var pipeline = new List<BsonDocument>
+        {
+            // Match documents based on the filter
+            new BsonDocument("$match", filter.ToBsonDocument()),
+
+            // Group by the time interval and calculate the average value of the selected measurement type
+            new BsonDocument("$group", new BsonDocument
+            {
+                { "_id", new BsonDocument
+                    {
+                        { "$dateTrunc", new BsonDocument
+                            {
+                                { "date", "$date" },
+                                { "unit", interval == "4hours" ? "hour" : interval }, // Use "hour" for both hour and 4-hour intervals
+                                { "binSize", interval == "4hours" ? 4 : 1 }  // If 4 hours, set binSize to 4
+                            }
+                        }
+                    }
+                },
+                { "averageMeasurement", new BsonDocument { { "$avg", $"${measurementField}" } } },  // Average value of the selected field
+                { "measurements", new BsonDocument { { "$push", "$$ROOT" } } }  // Optionally push all matching measurements into an array
+            })
+        };
+
+            // Execute the aggregation query
+            var measurements = await _measurementsCollection.Aggregate<BsonDocument>(pipeline).ToListAsync();
 
             if (measurements == null || !measurements.Any())
             {
@@ -67,7 +123,12 @@ public class MeasurementService : IMeasurementService
             }
             else
             {
-                resp.Measurements = measurements;
+                // Map the aggregated data into your response model
+                resp.Measurements = measurements.Select(m => new MeasurementProjection
+                {
+                    Date = m["_id"].ToUniversalTime(),  // The grouped date
+                    Value = m["averageMeasurement"].AsDecimal  // The average value for the selected measurement type
+                }).ToList();
             }
         }
         catch (Exception ex)
@@ -77,6 +138,7 @@ public class MeasurementService : IMeasurementService
         }
         return resp;
     }
+
 
     //Gets the measurements for specific station for the requested period
     //A valid day is from 00:00 - 23:59
